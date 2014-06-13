4e40d49d009578966416e2b1fb1e1cec2239855c
benjamin maple <rationalwasp@gmail.com>
2014-06-13 03:05:27 -0400

    problem solved

---
+++ p4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+def sq(x):
+    return x*x
+def palidromes(minNum, maxNum):
+    largestProduct = sq(minNum)
+    smallestProduct = sq(maxNum)
+    for number in range(999, 100, -1):
+        palindron = int(str(number) + str(number)[::-1])
+        for i in range(100, 999): 
+            if 100 <= palindron / i <= 999 and i * (palindron/i) == palindron: #wrong you dumbass
+                print i, palindron / i 
+                return palindron
+def main():
+   print palidromes(100, 999)
+if __name__ == '__main__':
+    main()
