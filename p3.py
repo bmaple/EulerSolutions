d5e88f7b2eb9150cdf52507e0ae8e403bc456ee3
benjamin maple <rationalwasp@gmail.com>
2014-06-12 23:20:35 -0400

    problem solved

--- p3.py
+++ p3.py
@@ -7,23 +7,18 @@ def siv(num):
     nums = [True] * int(math.sqrt(num))  
     for i in range (2, int(math.sqrt(num))): 
         if nums[i] == True:
-            for s in range(sq(i), int(math.sqrt(num)), i): #issue is here need to make it increment by i
+            for s in range(sq(i), int(math.sqrt(num)), i): 
                 nums[s] = False
     return nums 
 
 def biggestPrime(num):
     primes = siv(num)
-    largestIndex = 0
-    for index, i in enumerate(primes, start = 1):
-        '''if i == True and index > largestIndex and num % index == 0:
-            largestIndex = index
-    return largestIndex'''
-        if i == True and num % index == 0:
-            print(index)
+    
+    for index, val in reversed(list(enumerate(primes))):
+        if val and num % index == 0: return index
 
 def main():
     lP = 600851475143
-    #print(biggestPrime(lP))
-    biggestPrime(lP)
+    print(biggestPrime(lP))
 if __name__ == '__main__':
     main()
