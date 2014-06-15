d07ad485a29742dafdf6e38926a32b3b74143025
benjamin maple <rationalwasp@gmail.com>
2014-06-14 23:08:11 -0400

    cleaned

--- p4.py
+++ p4.py
@@ -7,7 +7,7 @@ def palidromes(minNum, maxNum):
     for number in range(999, 100, -1):
         palindron = int(str(number) + str(number)[::-1])
         for i in range(100, 999): 
-            if 100 <= palindron / i <= 999 and i * (palindron/i) == palindron: #wrong you dumbass
+            if 100 <= palindron / i <= 999 and i * (palindron/i) == palindron: 
                 print i, palindron / i 
                 return palindron
 def main():
